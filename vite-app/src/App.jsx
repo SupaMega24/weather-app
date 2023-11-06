cd3f266b207fcc28f753dbc86719b4e8d1b42cf3
noobslearn <138974329+SupaMega24@users.noreply.github.com>
2023-11-07 00:48:26 +0900

    Add header layout

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,4 +1,26 @@
-import './index.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div id='root' className='App'>
+      <header className='bg-gray- text-white text-center py-4'>
+        <h1>Charlie's Weather App</h1>
+      </header>
+
+      <form id='weatherForm' className='flex justify-center pb-4'>
+        <label for='cityInput' className='mr-2'>City Name:</label>
+        <button type='submit' className='bg-gray=800 text-white px-4 py-2'>Search</button>
+      </form> 
+      <div id='weatherDisplay' className='p-4 border border-gray-300'>
+
+      </div>
+    </div>
+  );  
+}
+
+export default App;
+
 
 
 
